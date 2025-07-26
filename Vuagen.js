d626cfc548e5ff5b795733de50c4ff444115f478
ZyFoxX <167752893+ZyFoxX@users.noreply.github.com>
2025-07-27 05:09:19 +0700

    Update Vuagen.js

--- Vuagen.js
+++ Vuagen.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const { parseArgs, showHelp } = require('./src/cli/parseArgs');
 const { UAGenerators, generateRandomUA } = require('./src/core/uagenerators');
